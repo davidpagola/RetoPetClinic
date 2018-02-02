5f78e389665eceeb62570a34eb336a050ad7e19a
kurotote <joseluisdelgadomiguez@gmail.com>
2018-02-02 12:53:40 +0100

    componentes offerts(edits)

--- spring-petclinic-angular/src/app/offerts/offert-edit/offert-edit.component.ts
+++ spring-petclinic-angular/src/app/offerts/offert-edit/offert-edit.component.ts
@@ -1,15 +1,50 @@
 import { Component, OnInit } from '@angular/core';
-
+import {OffertService} from '../offert.service';
+import {Offert} from '../offert';
+import {ActivatedRoute, Router} from '@angular/router';
+import 'rxjs/Rx';
 @Component({
   selector: 'app-offert-edit',
   templateUrl: './offert-edit.component.html',
   styleUrls: ['./offert-edit.component.css']
 })
 export class OffertEditComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+  public offert;
+  errorMessage: string;
+  constructor(private offertService: OffertService, private route: ActivatedRoute, private router: Router) { 
+    this.offert= <Offert>{};
   }
 
+  ngOnInit() {
+        const offertId = this.route.snapshot.params['id'];
+    this.offertService.getOffertById(offertId).subscribe(
+      offert => this.offert = offert,
+      error => this.errorMessage = <any> error);
+  }
+  onSubmit(offert) {
+    var that = this;
+    this.offertService.updateOffert(offert.id.toString(), offert).subscribe(
+      get_result,
+      get_error
+    );
+
+      function get_error(error) {
+      console.log(error);
+      console.log('error catched');
+      return this.errorMessage = <any> error;
+      }
+      function get_result(update_status) {
+      console.log(update_status);
+      if (update_status.status === 204) {
+        console.log('update success');
+        that.gotoOffertDetail(offert);
+      } else {
+        return console.log('update failed');
+      }
+    }
+  };
+
+    gotoOffertDetail(offert: Offert) {
+    this.router.navigate(['/owners', offert.id]);
+  }
 }
--- spring-petclinic-angular/src/app/offerts/offert-list/offert-list.component.ts
+++ spring-petclinic-angular/src/app/offerts/offert-list/offert-list.component.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit } from '@angular/core';
-
+import {Component, OnInit} from '@angular/core';
+import {OffertService} from '../offert.service';
+import {Offert} from '../offert';
+import 'rxjs/Rx';
+import {Router} from '@angular/router';
 @Component({
   selector: 'app-offert-list',
   templateUrl: './offert-list.component.html',
   styleUrls: ['./offert-list.component.css']
 })
 export class OffertListComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+  errorMessage: string;
+  offerts: Offert[];
+  constructor(private router: Router, private offertService: OffertService) { 
   }
 
+  ngOnInit() {
+    this.offertService.getOfferts().subscribe(
+      offerts => this.offerts = offerts,
+      error => this.errorMessage = <any> error);
+  }
+  onSelect(offert: Offert) {
+    this.router.navigate(['/offert', offert.id]);
+  }
+  addOffert() {
+    this.router.navigate(['/offerts/add']);
+  }
 }
